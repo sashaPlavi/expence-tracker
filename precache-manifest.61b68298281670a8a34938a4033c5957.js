f32dcda278fe72c304759ed5cc42ea356654031e
SashaPlavi <sasha.bingulac@gmail.com>
2020-02-23 21:54:14 +0100

    Updates

---
+++ precache-manifest.61b68298281670a8a34938a4033c5957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44de23eba437428b860f4f14c1762453",
+    "url": "/sashaPlavi/expence-tracker/index.html"
+  },
+  {
+    "revision": "e0630e933b94a228b87e",
+    "url": "/sashaPlavi/expence-tracker/static/css/main.1515c12b.chunk.css"
+  },
+  {
+    "revision": "f8d1353c6ff1e6be919f",
+    "url": "/sashaPlavi/expence-tracker/static/js/2.dfc8c839.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sashaPlavi/expence-tracker/static/js/2.dfc8c839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0630e933b94a228b87e",
+    "url": "/sashaPlavi/expence-tracker/static/js/main.02e6a370.chunk.js"
+  },
+  {
+    "revision": "2a2e2d27c3ab0d98324c",
+    "url": "/sashaPlavi/expence-tracker/static/js/runtime-main.dbd31902.js"
+  }
+]);
